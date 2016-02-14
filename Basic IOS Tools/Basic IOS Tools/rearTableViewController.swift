3f5c654ce5e2fe24a4b7472cb214dea529653737
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-13 17:16:05 -0800

    Cell identifier updated
    Cell identifier in rearTableViewController has been updated to match
    the tableArray index path.

--- Basic IOS Tools/Basic IOS Tools/rearTableViewController.swift
+++ Basic IOS Tools/Basic IOS Tools/rearTableViewController.swift
@@ -23,7 +23,7 @@ class rearTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        var cell = tableView.dequeueReusableCellWithIdentifier(tableArray[indexPath.row], forIndexPath: indexPath) as! UITableViewCell
         
         cell.textLabel?.text = tableArray[indexPath.row]
         
