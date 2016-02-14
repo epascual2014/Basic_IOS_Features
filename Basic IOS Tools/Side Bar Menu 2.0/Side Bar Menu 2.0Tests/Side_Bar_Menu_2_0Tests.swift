f5b4c40e903493344730b3411cf1ab2376bf05e0
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-13 21:48:28 -0800

    New Side Bar Menu Project
    Created a simpler side bar menu project.

---
+++ Basic IOS Tools/Side Bar Menu 2.0/Side Bar Menu 2.0Tests/Side_Bar_Menu_2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Side_Bar_Menu_2_0Tests.swift
+//  Side Bar Menu 2.0Tests
+//
+//  Created by Edrick Pascual on 2/13/16.
+//  Copyright (c) 2016 Edrick Pascual. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Side_Bar_Menu_2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
