bf61ca3a384cba18b4c8a2e02b78da9f1e89204a
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-13 22:13:04 -0800

    MenuButton outlet created
    Menu Button outlet created and panGestureRecognizer added to make menu
    Item working.

--- Basic IOS Tools/Side Bar Menu 2.0/Side Bar Menu 2.0/MainViewController.swift
+++ Basic IOS Tools/Side Bar Menu 2.0/Side Bar Menu 2.0/MainViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            
+            // Will call selector located in SWReveal File
+            menuButton.action = Selector("revealToggle:")
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +33,5 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
